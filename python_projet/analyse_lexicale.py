e5191c6b17e1beed12b07dc8202b2ac8d4e59895
ClervieCauser <clervie.causer@etu.unice.fr>
2023-03-24 16:16:10 +0100

    comparateur

--- python_projet/analyse_lexicale.py
+++ python_projet/analyse_lexicale.py
@@ -3,18 +3,23 @@ from sly import Lexer
 
 class FloLexer(Lexer):
 	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL }
+	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL, EGAL, INFRIEUR, SUPERIEUR, SUPERIEUR_OU_EGAL, DIFFERENT }
 
 	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
 	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
 	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
 	literals = { '+','*','(',')',";" }
 	
-	# chaines contenant les caractère à ignorer. Ici espace et tabulation
+	# chaines contenant les caractères à ignorer. Ici espace et tabulation
 	ignore = ' \t'
 
-	# Expressions régulières correspondant au différents Lexèmes par ordre de priorité
+	# Expressions régulières correspondant aux différents Lexèmes par ordre de priorité
+	EGAL= r'=='
+	INFERIEUR= r'<'
 	INFERIEUR_OU_EGAL= r'<='
+	SUPERIEUR= r'>'
+	SUPERIEUR_OU_EGAL= r'>='
+	DIFFERENT= r'!='
 	
 	@_(r'0|[1-9][0-9]*')
 	def ENTIER(self, t):

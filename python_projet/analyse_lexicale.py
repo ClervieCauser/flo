b392f3187e806faf50cd8f19d08ff9c657c43a8e
ClervieCauser <clervie.causer@etu.unice.fr>
2023-05-26 16:49:31 +0200

    expression booleennes (ne marche pas encore)

--- python_projet/analyse_lexicale.py
+++ python_projet/analyse_lexicale.py
@@ -2,74 +2,74 @@ import sys
 from sly import Lexer
 
 class FloLexer(Lexer):
-	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, BOOLEAN, ECRIRE , SI, SINON_SI, SINON, TANT_QUE, RETOURNER, INFERIEUR_OU_EGAL, EGAL, INFERIEUR, SUPERIEUR, SUPERIEUR_OU_EGAL, DIFFERENT, ET, OU, NON, LIRE }
+    # Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
+    tokens = { IDENTIFIANT, ENTIER, BOOLEAN, ECRIRE , SI, SINON_SI, SINON, TANT_QUE, RETOURNER, INFERIEUR_OU_EGAL, EGAL, INFERIEUR, SUPERIEUR, SUPERIEUR_OU_EGAL, DIFFERENT, ET, OU, NON, LIRE, VRAI, FAUX }
 
-	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
-	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
-	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
-	literals = { '+','*','(',')',";", "/","%","-" }
-	
-	# chaines contenant les caractères à ignorer. Ici espace et tabulation
-	ignore = ' \t'
+    #Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale.
+    #Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
+    #Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
+    literals = { '+','*','(',')',";", "/","%","-" }
 
-	# Expressions régulières correspondant aux différents Lexèmes par ordre de priorité
-	#arithmetique
+    # chaines contenant les caractères à ignorer. Ici espace et tabulation
+    ignore = ' \t'
 
-	#comparateur
-	EGAL= r'=='
-	INFERIEUR= r'<'
-	INFERIEUR_OU_EGAL= r'<='
-	SUPERIEUR= r'>'
-	SUPERIEUR_OU_EGAL= r'>='
-	DIFFERENT= r'!='
+    # Expressions régulières correspondant aux différents Lexèmes par ordre de priorité
+    #arithmetique
 
-	#logique
-	ET= r'&'
-	OU= r'\|'
-	NON= r'not'
-	
-	@_(r'0|[1-9][0-9]*')
-	def ENTIER(self, t):
-		t.value = int(t.value)
-		return t
+    #comparateur
+    EGAL= r'=='
+    INFERIEUR= r'<'
+    INFERIEUR_OU_EGAL= r'<='
+    SUPERIEUR= r'>'
+    SUPERIEUR_OU_EGAL= r'>='
+    DIFFERENT= r'!='
 
-	@_(r'vrai|faux|1|0')
-	def BOOLEAN(self, t):
-		t.value = bool(t.value)
-		return t
+    #logique
+    ET= r'&'
+    OU= r'\|'
+    NON= r'not'
 
-    	# cas général
-	IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*' #en général, variable ou nom de fonction
+    @_(r'0|[1-9][0-9]*')
+    def ENTIER(self, t):
+        t.value = int(t.value)
+        return t
 
-	# cas spéciaux:
-	IDENTIFIANT['ecrire'] = ECRIRE
-	IDENTIFIANT['si'] = SI
-	IDENTIFIANT['sinon si'] = SINON_SI
-	IDENTIFIANT['sinon'] = SINON
-	IDENTIFIANT['tant que'] = TANT_QUE
-	IDENTIFIANT['retourner'] = RETOURNER
-	IDENTIFIANT['lire'] = LIRE
-	
-	#Syntaxe des commentaires à ignorer
-	ignore_comment = r'\#.*'
+    @_(r'VRAI|FAUX|1|0')
+    def BOOLEAN(self, t):
+        t.value = bool(t.value)
+        return t
 
-	# Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
-	@_(r'\n+')
-	def ignore_newline(self, t):
-		self.lineno += t.value.count('\n')
+    # cas général
+    IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*' #en général, variable ou nom de fonction
 
-	# En cas d'erreur, indique où elle se trouve
-	def error(self, t):
-		print(f'Ligne{self.lineno}: caractère inattendu "{t.value[0]}"')
-		self.index += 1
+    # cas spéciaux:
+    IDENTIFIANT['ecrire'] = ECRIRE
+    IDENTIFIANT['si'] = SI
+    IDENTIFIANT['sinon si'] = SINON_SI
+    IDENTIFIANT['sinon'] = SINON
+    IDENTIFIANT['tant que'] = TANT_QUE
+    IDENTIFIANT['retourner'] = RETOURNER
+    IDENTIFIANT['lire'] = LIRE
+
+    #Syntaxe des commentaires à ignorer
+    ignore_comment = r'\#.*'
+
+    # Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    # En cas d'erreur, indique où elle se trouve
+    def error(self, t):
+        print(f'Ligne{self.lineno}: caractère inattendu "{t.value[0]}"')
+        self.index += 1
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
-	else:
-		with open(sys.argv[1],"r") as f:
-			data = f.read()
-			lexer = FloLexer()
-			for tok in lexer.tokenize(data):
-				print(tok)
+    if len(sys.argv) < 2:
+        print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
+    else:
+        with open(sys.argv[1],"r") as f:
+            data = f.read()
+            lexer = FloLexer()
+            for tok in lexer.tokenize(data):
+                print(tok)
